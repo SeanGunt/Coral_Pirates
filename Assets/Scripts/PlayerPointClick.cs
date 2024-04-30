a2794427e95c46ae788657f5ec1088cf897ef41b
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2024-04-30 14:53:28 -0400

    Fungus Destroyed

--- Assets/Scripts/PlayerPointClick.cs
+++ Assets/Scripts/PlayerPointClick.cs
@@ -6,13 +6,10 @@ using UnityEngine.AI;
 public class PlayerPointClick : MonoBehaviour
 {
     public Vector2 target;
-
     private NavMeshAgent agent;
-
     public float minScale = 0.6f;  // Minimum scale factor
     public float maxScale = 1.0f;  // Maximum scale factor
     public float scaleThreshold = 20.0f;  // Where should sprite be at max scale
-
     public bool inDialogue;
 
     private void Start()
@@ -33,9 +30,7 @@ public class PlayerPointClick : MonoBehaviour
             agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
         
             AdjustPerspective();
-
         }
-        
     }
 
     private void HandleMouseInput()
@@ -56,12 +51,7 @@ public class PlayerPointClick : MonoBehaviour
 
         transform.localScale = new Vector3(targetScale, targetScale, 1);
     }
-
-     /*private void OnCollisionStay2D(Collision2D collision)
-    {
-        target = transform.position;
-    }*/
-
+    
     public void ExitDialogue()
     {
         inDialogue = false;
