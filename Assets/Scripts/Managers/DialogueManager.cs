f3f7594c325d0c604c300787c07b0bf8e55a2175
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2024-05-17 17:35:00 -0400

    dialogue now scrolls

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -10,6 +10,11 @@ public class DialogueManager : MonoBehaviour
     public GameObject dialogueCanvas;
     public TextMeshProUGUI masterText;
     public Clickable currentClickable;
+    public float timeBetweenCharacters;
+    public bool typeWriteActive;
+    int visibleCount;
+    int totalVisibleCharacters;
+    int counter;
     private void Awake()
     {
         instance = this;
@@ -26,14 +31,22 @@ public class DialogueManager : MonoBehaviour
         inDialogue = true;
         currentClickable = clickableToSet;
         masterText.text = dialogueToSet[0];
+        StartCoroutine(TextVisible());
     }
 
     public void ContinueDialogue()
     {
-        if (currentClickable.index + 1 < currentClickable.dialogue.Count)
+        if (typeWriteActive)
+        {
+            counter = totalVisibleCharacters;
+            return;
+        }
+
+        if (currentClickable.index + 1 < currentClickable.dialogue.Count && !typeWriteActive)
         {
             currentClickable.index++;
             masterText.text = currentClickable.dialogue[currentClickable.index];
+            StartCoroutine(TextVisible());
         }
         else
         {
@@ -47,5 +60,28 @@ public class DialogueManager : MonoBehaviour
         inDialogue = false;
         currentClickable.index = 0;
         currentClickable = null;
+    }
+
+    private IEnumerator TextVisible()
+    {
+        masterText.ForceMeshUpdate();
+        totalVisibleCharacters = masterText.textInfo.characterCount;
+        counter = 0;
+        typeWriteActive = true;
+
+        while (true)
+        {
+            visibleCount = counter % (totalVisibleCharacters + 1);
+            masterText.maxVisibleCharacters = visibleCount;
+
+            if (visibleCount >= totalVisibleCharacters)
+            {
+                typeWriteActive = false;
+                break;
+            }
+
+            counter += 1;
+            yield return new WaitForSeconds(timeBetweenCharacters);
+        }
     }
 }
